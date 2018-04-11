eb10f1d2a3d90d5995b06ce03fac8c142c3651aa
DmitryIlin <339966289@mail.ru>
2018-04-11 11:33:50 +0300

    Users should be able to upload a profile photo for their accounts, issue #226

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -34,7 +34,7 @@ RSpec.configure do |config|
 
   config.after(:each) do
     if Rails.env.test? || Rails.env.cucumber?
-      FileUtils.rm_rf(Dir["#{Rails.root}/spec/support/uploads"])
+      FileUtils.rm_rf(Dir["#{Rails.root}/tmp/uploads"])
     end
   end
 end
